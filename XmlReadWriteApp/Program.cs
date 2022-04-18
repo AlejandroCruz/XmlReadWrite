08b9af6eab9e3247987923708f8a9061cce839a4
Alejandro Cruz <AlejandroCruz@users.noreply.github.com>
2022-04-18 17:19:56 -0400

    Add project files.

---
+++ XmlReadWriteApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var xmlHandler = new XmlHandler(@"C:\...\xmlFile.xml");
+string elementName = "title";
+
+IEnumerable<string> listOfTitles = xmlHandler.GetElementList(elementName);
+
+foreach (string title in listOfTitles)
+    Console.WriteLine(title);
---
+++ XmlReadWriteApp/XmlHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Linq;
+
+internal class XmlHandler
+{
+    public XmlHandler(string xmlSource)
+    {
+        XmlSource = xmlSource;
+    }
+
+    public string XmlSource { get; }
+
+    internal IEnumerable<string> GetElementList(string elementName)
+    {
+        XElement xElement = XElement.Load(XmlSource);
+
+        IEnumerable<string> list = from eName in xElement.Descendants(elementName)
+                                   select eName.Value;
+
+        return list;
+    }
+}
